06eeedff0a5e9c652b3eb46b3823080af0fd2a72
Buster <bustercrow@live.com>
2025-07-28 07:28:47 -0500

    still learning dicts

--- scripts/classes/shelf.gd
+++ scripts/classes/shelf.gd
@@ -1,12 +1,16 @@
 extends Node2D
 class_name Shelf
 
-@export var shelf_type: StoreStock.sections
+@export var shelf_type: String
 
-var poss_contents: Dictionary = StoreStock.shelf_type.duplicate()
+var poss_contents: Dictionary = {"Fruit": 3, "Veggies": 2}
 var contents: Dictionary
 
 func _ready() -> void:
+	set_contents()
+	for i in contents:
+		print(contents.i.key())
+		print(contents.i)
 	pass
 
 func set_contents() -> void:
