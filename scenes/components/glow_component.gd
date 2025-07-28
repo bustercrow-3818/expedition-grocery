5f8e7ccf6501b0517a00a5b913db0a69f7105601
Buster <bustercrow@live.com>
2025-07-27 22:54:56 -0500

    Adjusted detection for glow component

--- scenes/components/glow_component.gd
+++ scenes/components/glow_component.gd
@@ -12,17 +12,18 @@ var player_near: bool = false
 func _ready() -> void:
 	player_det.connect("body_entered", detect_player_near)
 	player_det.connect("body_exited", detect_player_far)
-	mouse_det.connect("mouse_entered", glow)
-	mouse_det.connect("mouse_exited", glow)
+	mouse_det.connect("mouse_entered", detect_mouse_near)
+	mouse_det.connect("mouse_exited", detect_mouse_far)
 	pass
 
+func _physics_process(_delta: float) -> void:
+	glow()
+
 func glow() -> void:
-	if hover == true:
-		hover = false
-		object_mod.modulate = Color(1, 1, 1, 1)
-	elif player_near == true:
-		hover = true
+	if hover == true and player_near == true:
 		object_mod.modulate = Color(0, 1, 0, 1)
+	else:
+		object_mod.modulate = Color(1, 1, 1, 1)
 
 func detect_player_near(body: Node2D) -> void:
 	if body is Player:
@@ -36,3 +37,8 @@ func detect_player_far(body: Node2D) -> void:
 	else:
 		return
 	
+func detect_mouse_near() -> void:
+	hover = true
+	
+func detect_mouse_far() -> void:
+	hover = false
