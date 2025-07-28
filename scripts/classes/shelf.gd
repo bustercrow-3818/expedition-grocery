61c82174d8b6839a5634080e904ba97edc046e9d
Buster <bustercrow@live.com>
2025-07-28 06:39:29 -0500

    barely anything done

--- scripts/classes/shelf.gd
+++ scripts/classes/shelf.gd
@@ -4,11 +4,15 @@ class_name Shelf
 @export var shelf_type: StoreStock.sections
 
 var poss_contents: Dictionary = StoreStock.shelf_type.duplicate()
-var contents: Array[String]
+var contents: Dictionary
 
 func _ready() -> void:
-	poss_contents = StoreStock.shelf_type.keys()
+	pass
 
-func set_contents(variety: int, qty: int) -> void:
-	
+func set_contents() -> void:
+	var check: int
+	for i in poss_contents:
+		check = randi_range(0, 1)
+		if check > 0:
+			contents[i] = i
 	pass
